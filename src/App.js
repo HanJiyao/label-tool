d667a2f03980e41493ce24170271da299694074d
HanJiyao <hanjiyao0@gmail.com>
2019-03-18 18:08:22 +0800

    read csv and search index

--- src/App.js
+++ src/App.js
@@ -1,95 +1,156 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Input} from 'react-materialize'
 const divStyle = {
-  marginTop:"5rem",
-  textAlign:"center"
+  textAlign:"center",
+  marginTop:"5rem"
 };
 const contentStyle={
   textAlign:"left",
-  marginTop:"3rem",
 };
 const btnStyle = {
-  margin:"0.5rem 1rem 0  0",
+  width:"100%",
+  marginTop:".5rem"
 };
 const actStyle = {
-  paddingTop:"3rem",
   textAlign:"left"
 }
 const rowStyle = {
-  marginBottom:"0"
+  paddingTop:"1rem",
+  marginBottom:"0",
 }
 const arrowStyle={
   color:"white",
-  marginTop:"12rem"
+  lineHeight:"36rem"
 }
 const arrowTxtStyle={
   fontSize:"2rem",
 }
+const hideStyle={
+  display:'none'
+}
+const indexStyle={
+  width:"10rem",
+  margin:"auto",
+}
+const colStyle={
+  paddingTop:"2rem"
+}
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'coconut'};
+    this.state = {
+      topic: 'test',
+      correct: true,
+      index: 0
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setCorrect = this.setCorrect.bind(this);
+    this.setFalse = this.setFalse.bind(this);
+    this.updateData = this.updateData.bind(this);
+    this.index = this.index.bind(this);
+    this.indexDown = this.indexDown.bind(this);
+    this.indexUp = this.indexUp.bind(this);
+  }
+  setCorrect(){
+    this.setState({correct:true});
+  }
+  setFalse(){
+    this.setState({correct:false});
   }
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({topic: event.target.value});
   }
   handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
+    alert('The new topic saved as: ' + this.state.topic);
     event.preventDefault();
   }
-  componentDidMount () {
-    const s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.async = true;
-    s.innerHTML = "document.addEventListener('DOMContentLoaded', function() {var elems = document.querySelectorAll('select');var instances = M.FormSelect.init(elems); });";
-    this.instance.appendChild(s);
+  index(event){
+    var index = parseInt(event.target.value)
+    if(index>this.state.data.length ){
+      this.setState({index: this.state.data.length-1});
+    } else if (index < 0 || isNaN(index)) {
+      this.setState({index: 0});
+    } else {
+      this.setState({index: index}, () => {
+        console.log(this.state.index);
+      })
+    }
+  }
+  updateData(result) {
+    const data = result.data;
+    this.setState({data: data}); 
+  }
+  indexUp(){
+    this.setState({index: this.state.index+1}); 
+  }
+  indexDown(){
+    this.setState({index: this.state.index-1}); 
+  }
+  componentWillMount() {
+    var csvFilePath = require("./all_items_C0014376286P_label.csv");
+    var Papa = require("papaparse/papaparse.min.js");
+    Papa.parse(csvFilePath, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      complete: this.updateData
+    });
   }
   render() {
-      // var XLSX = require('xlsx');
-      // var workbook = XLSX.readFile('all_items_NedbankTest_label.xlsx');
-      // var first_sheet_name = workbook.SheetNames[0];
-      // var address_of_cell = 'D5';
-      // /* Get worksheet */
-      // var worksheet = workbook.Sheets[first_sheet_name];
-      // /* Find desired cell */
-      // var desired_cell = worksheet[address_of_cell];
-      // /* Get the value */
-      // var desired_value = desired_cell.v;
-    return (
-      <div className="container ">
-        <div className="card row" style={divStyle} ref={el => (this.instance = el)}>
-          <div className="col s2" style={arrowStyle}><button className=" btn-floating btn-large waves-effect waves-light"><i style={arrowTxtStyle} className="material-icons">keyboard_arrow_left</i></button></div>
-          <div className="col s8">
-          
-            <div className="card-content" style={contentStyle}>
-              <h4>Content Title</h4>
-              <br/>
-              <h5>This is the content</h5>
-            </div>
-            <div className="card-action" style={actStyle}>
-              <div className="row" style={rowStyle}>
-                <div className="col l6 m12">
-                  <button style={btnStyle} className="waves-effect waves-light btn-large"><i className="material-icons left">check</i>yes</button>
-                  <button style={btnStyle} className="waves-effect waves-light btn-large"><i className="material-icons left">clear</i>no</button>
+    if(this.state.data!==undefined){
+      return (
+        <div className="container">
+          <div className="card" style={divStyle} >
+            <div className="row" style={rowStyle}>
+              <div className="col s2"  style={arrowStyle}><button onClick={this.indexDown} disabled={this.state.index===0?'disabled' : null} className=" btn-floating btn-large waves-effect waves-light blue"><i style={arrowTxtStyle} className="material-icons">keyboard_arrow_left</i></button></div>
+              <div className="col s8" style={colStyle}>
+                <div className="input-field" style={indexStyle}>
+                  <i className="material-icons prefix">search</i>
+                  <input type="number" id="index" value={this.state.index} onChange={this.index}/>
+                  <label htmlFor="index" className="active">Index</label>
                 </div>
-                <div className="input-field col l6 m12" >
-                  <select value={this.state.value} onChange={this.handleChange}>
-                    <option value="grapefruit">Grapefruit</option>
-                    <option value="lime">Lime</option>
-                    <option value="coconut">Coconut</option>
-                    <option value="mango">Mango</option>
-                  </select>
-                  <label>Topics</label>
+                <div className="card-content" style={contentStyle}>
+                  <h4>{(this.state.data[this.state.index].Title!==undefined)?this.state.data[this.state.index].Title:"Invalid Index"}</h4>
+                  <h5>{(this.state.data[this.state.index].Description!==undefined)?this.state.data[this.state.index].Description:"Invalid Index"})</h5>
+                </div>
+                <div className="card-action" style={actStyle}>
+                  <div className="row" style={rowStyle}>
+                    <h5 className="col l6 m12">Topic: {this.state.data[this.state.index].Topic}</h5>
+                    <div className="col l6 m12">
+                      <div className="row">
+                        <div className="col s6">
+                          <button style={btnStyle} className={this.state.correct ? 'waves-effect waves-light btn-large green': 'waves-effect waves-light btn-large grey'} onClick={this.setCorrect}><i className="material-icons left">check</i>yes</button>
+                        </div>
+                        <div className="col s6">
+                          <button style={btnStyle} className={this.state.correct ? 'waves-effect waves-light btn-large grey': 'waves-effect waves-light btn-large red'} onClick={this.setFalse}><i className="material-icons left">clear</i>no</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row" style={this.state.correct ? hideStyle : rowStyle}>
+                    <Input l={6} m={12} type='select' label="New Topic" onChange={this.handleChange}>
+                      <option value='1'>Option 1</option>
+                      <option value='2'>Option 2</option>
+                      <option value='3'>Option 3</option>
+                    </Input>
+                    <div className="col l6 m12">
+                      <button style={btnStyle} className="waves-effect waves-light btn-large blue" onClick={this.handleSubmit}><i className="material-icons left">save</i>save</button>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <div className="col s2" style={arrowStyle}><button onClick={this.indexUp} disabled={this.state.index===this.state.data.length-1?'disabled' : null} className="btn-floating btn-large waves-effect waves-light blue"><i style={arrowTxtStyle} className=" material-icons">keyboard_arrow_right</i></button></div>
             </div>
           </div>
-          <div className="col s2" style={arrowStyle}><button className="btn-floating btn-large waves-effect waves-light"><i style={arrowTxtStyle} className=" material-icons">keyboard_arrow_right</i></button></div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return(
+        <div>Hello</div>
+      )
+    }
   }
 }
 
