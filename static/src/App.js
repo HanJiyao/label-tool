c89bfff93e44d249332ce2517bc6aebc35ff6f93
HanJiyao <hanjiyao0@gmail.com>
2019-03-22 10:43:41 +0800

    add flask as server

--- static/src/App.js
+++ static/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import Select from 'react-select';
 import load from './load.gif';
 import update from 'react-addons-update'; 
-import {CSVLink} from 'react-csv'
+import { CSVLink } from 'react-csv'
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+registerPlugin();
 
-const fileName = 'all_items_C0014376286P.csv'
+const fileName = 'all_items_C0014376286P_label.csv'
 const options = [
   { value: '', label: 'blank/undetermined' },
   { value: 'communication', label: 'communication' },
@@ -18,90 +21,29 @@ const options = [
   { value: 'project management', label: 'project management' },
   { value: 'prodict planning', label: 'prodict planning' },
 ]
-const customStyles = {
-  option: (provided, state) => ({
-    ...provided,
-  })
-}
-const divStyle = {
-  textAlign:"center",
-  marginTop:"5vh",
-  padding:"0",
-};
-const contentStyle={
-  textAlign:"left",
-  margin:"0",
-  paddingTop:".5rem"
-};
-const btnStyle = {
-  width:"100%",
-  height:'65px',
-  borderRadius:'100px',
-  zIndex:"0",
-  fontSize:"1.5rem",
-  fontWeight:"600"
-};
-const symbolStyle={
-  fontSize:"2rem",
-  fontWeight:"900",
-  margin:"0"
-}
-const actStyle = {
-  textAlign:"left",
-  paddingTop:"0",
-}
-const rowStyle = {
-  paddingTop:"1.5rem",
-  margin:"0",
-}
-const arrowStyle={
-  color:"white"
-}
-const arrowTxtStyle={
-  fontSize:"3rem",
-  margin:"0"
-}
-const hideStyle={
-  display:'none'
-}
-const indexStyle={
-  width:"10rem",
-}
-const loadingStyle={
-  height:'100vh',
-  width:'100vw',
-  position:'relative',
-}
-const loadImg={
-  position:'absolute',
-  top: '42%',
-  left: '48%',
-}
-const red={
-  color:"red",
-}
-const saveBtn={
-  borderRadius:"100px",
-  marginTop:".6rem",
-  width:"100%",
-  marginBottom:"1rem"
-}
-const selectStyle={
-  paddingTop:"2rem",
-  margin:"0",
-  height:"100px"
-}
-const mainRowStyle={
-  paddingTop:"1rem",
-  width:"100%"
-}
-const descStyle={
-  height:"15vh",
-  overflowY:"scroll"
-}
-const titleStyle={
-  height:"4rem",
-  overflowY:"scroll"
+
+const inlineStyle = {
+  customStyles:{
+    option: (provided, state) => ({
+      ...provided,})
+  },
+  divStyle : {textAlign:"center",padding:"0",},
+  contentStyle:{textAlign:"left",margin:"0",paddingTop:".5rem"},
+  btnStyle:{width:"100%",height:'65px',borderRadius:'100px',zIndex:"0",fontSize:"1.5rem",fontWeight:"600"},
+  symbolStyle:{fontSize:"2rem",fontWeight:"900",margin:"0"},
+  actStyle:{textAlign:"left",paddingTop:"0",},
+  rowStyle:{paddingTop:"1.5rem",margin:"0",},
+  arrowStyle:{color:"white"},
+  arrowTxtStyle:{fontSize:"3rem",margin:"0"},
+  hideStyle:{display:'none'},
+  indexStyle:{width:"10rem",},
+  loadingStyle:{height:'100vh',width:'100vw',position:'relative',},
+  loadImg:{position:'absolute',top: '42%',left: '48%',},
+  red:{color:"red"},saveBtn:{borderRadius:"100px",marginTop:".6rem",width:"100%",marginBottom:"1rem"},
+  selectStyle:{paddingTop:"2rem",margin:"0",height:"100px"},
+  mainRowStyle:{paddingTop:"1rem",width:"100%"},
+  descStyle:{height:"15vh",overflowY:"scroll"},
+  titleStyle:{height:"4rem",overflowY:"scroll"}
 }
 class App extends Component {
   constructor(props) {
@@ -138,7 +80,7 @@ class App extends Component {
   }
   indexSearch(event){
     var index = parseInt(event.target.value)
-    if(index>this.state.data.length ){
+    if(index>this.state.data.length-1 ){
       this.setState({index: this.state.data.length-1},()=>{this.loadNewItem()});
     } else if (index < 0 || isNaN(index)) {
       this.setState({index: 0},()=>{this.loadNewItem()});
@@ -157,20 +99,21 @@ class App extends Component {
   }
   async indexUp(){
     await this.writeData()
-        if(this.state.index<this.state.data.length-1){
-          this.setState({index: this.state.index+1},()=>{this.loadNewItem()});
-        }
+    if(this.state.index<this.state.data.length-1){
+      this.setState({index: this.state.index+1},()=>{this.loadNewItem()});
+    }
   }
   async indexDown(){
     await this.writeData()
-      if(this.state.index>=0){
-        this.setState({index: this.state.index-1},()=>{this.loadNewItem();}); 
-      }
+    if(this.state.index>=0){
+      this.setState({index: this.state.index-1},()=>{this.loadNewItem();}); 
+    }
   }
   handleChange = (selectedOption) => {
     this.setState({topic:selectedOption.value});
   }
-  startDownload(){
+  async startDownload(){
+    await this.writeData()
     this.setState({download:true})
   }
   writeData(){
@@ -199,26 +142,22 @@ class App extends Component {
   componentWillUnmount(){
     document.removeEventListener("keydown", this.keyFunction, false);
   }
+  handleInit() {
+    console.log('FilePond instance has initialised', this.pond);
+  }
   keyFunction(event) {
     var key = event.keyCode || event.charCode || 0;
     var target = event.target || event.srcElement;
     var targetTagName = (target.nodeType === 1) ? target.nodeName.toUpperCase() : "";
     if ( !/INPUT|SELECT|TEXTAREA/.test(targetTagName) ){
       switch(key){
-        case 65: 
-          document.getElementById("arrowDown").click();break;
-        case 68: 
-          document.getElementById("arrowUp").click();break;
-        case 37: 
-          document.getElementById("arrowDown").click();break;
-        case 39: 
-          document.getElementById("arrowUp").click();break;
-        case 89: 
-          document.getElementById("yesBtn").click();break;
-        case 78: 
-          document.getElementById("noBtn").click();break;
-        case 13:
-          this.selectRef.focus();break;
+        case 65: document.getElementById("arrowDown").click();break;
+        case 68: document.getElementById("arrowUp").click();break;
+        case 37: document.getElementById("arrowDown").click();break;
+        case 39: document.getElementById("arrowUp").click();break;
+        case 89: document.getElementById("yesBtn").click();break;
+        case 78: document.getElementById("noBtn").click();break;
+        case 13:if(!this.state.correct) this.selectRef.focus();break;
         default: break;
       }
     }
@@ -228,53 +167,65 @@ class App extends Component {
       var currentTopic = this.state.data[this.state.index].Topic.replace(/[["\]]/g, '')
       return (
         <div className="container">
-          <div className="card valign-wrapper" style={divStyle} >
-            <div className="row valign-wrapper" style={mainRowStyle}>
-              <div className="col m2 hide-on-small-only"  style={arrowStyle}>
+        <FilePond ref={ref => this.pond = ref}
+              files={this.state.files}
+              allowMultiple={true}
+              maxFiles={3}
+              server="/api"
+              oninit={() => this.handleInit() }
+              onupdatefiles={(fileItems) => {
+                  this.setState({
+                      files: fileItems.map(fileItem => fileItem.file)
+                  });
+              }}>
+          </FilePond>
+          <div className="card valign-wrapper" style={inlineStyle.divStyle} >
+            <div className="row valign-wrapper" style={inlineStyle.mainRowStyle}>
+              <div className="col m2 hide-on-small-only"  style={inlineStyle.arrowStyle}>
                 <button id="arrowDown" 
                   onClick={this.indexDown} 
                   disabled={this.state.index===0?'disabled' : null} 
                   className=" btn-floating btn-large waves-effect waves-light blue">
-                    <i style={arrowTxtStyle} className="material-icons">keyboard_arrow_left</i>
+                    <i style={inlineStyle.arrowTxtStyle} className="material-icons">keyboard_arrow_left</i>
                 </button>
               </div>
-              <div className="col m8 s12" style={mainRowStyle}>
-                <div className="card-content row" style={contentStyle}>
-                  <div className="input-field col s12 m1" style={indexStyle}>
+              <div className="col m8 s12" style={inlineStyle.mainRowStyle}>
+                <div className="card-content row" style={inlineStyle.contentStyle}>
+                  <div className="input-field col s12 m1" style={inlineStyle.indexStyle}>
                     <i className="material-icons prefix">search</i>
                     <input type="number" id="index" value={this.state.index} onChange={this.indexSearch}/>
                     <label htmlFor="index" className="active">Index</label>
                   </div>
-                  <div className="col s12 m6 l5 right">
+                  <div className="col s12 m6 l4 right">
                     {this.state.download?
                     <CSVLink 
                       data={this.state.data} 
                       filename={fileName}
                       target="_blank">
-                      <button style={saveBtn} className="waves-effect waves-light btn-large green" onClick={this.startDownload}><i className="material-icons left">save_alt</i>Confirm</button>
+                      <button style={inlineStyle.saveBtn} className="waves-effect waves-light btn-large green" onClick={this.startDownload}><i className="material-icons left">save_alt</i>Confirm</button>
                     </CSVLink>
-                    :<button style={saveBtn} className="waves-effect waves-light btn-large" onClick={this.startDownload}><i className="material-icons left">save</i>Download</button>}
+                    :<button style={inlineStyle.saveBtn} className="waves-effect waves-light btn-large" onClick={this.startDownload}><i className="material-icons left">save</i>Download</button>}
                   </div>
                 </div>
-                <div className="card-content row" style={contentStyle}>
-                  <h5 id="customScroll"  className="col s12"  style={titleStyle}>{this.state.data[this.state.index].Title}</h5>
-                  <h6 id="customScroll" className="col s12" style={descStyle}>{this.state.data[this.state.index].Description}</h6>
+                <div className="card-content row" style={inlineStyle.contentStyle}>
+                  <h5 id="customScroll"  className="col s12" style={inlineStyle.titleStyle}>{this.state.data[this.state.index].Title}</h5>
+                  <h6 id="customScroll" className="col s12" style={inlineStyle.descStyle}>{this.state.data[this.state.index].Description}</h6>
                 </div>
-                <div className="card-action" style={actStyle}>
+                <div className="card-action" style={inlineStyle.actStyle}>
                   {(this.state.correct)?
-                    <div className="row" style={selectStyle}>
-                      <h5 className="col s12" style={currentTopic===""?red:null}>
+                    <div className="row" style={inlineStyle.selectStyle}>
+                      <h5 className="col s12" style={currentTopic===""?inlineStyle.red:null}>
                         <strong >{currentTopic===""?"Cannot Determined":currentTopic}</strong>
                       </h5>
                     </div>
                     :
-                    <div className="row" style={this.state.correct ? hideStyle : selectStyle}>
+                    <div className="row" style={this.state.correct ? inlineStyle.hideStyle : inlineStyle.selectStyle}>
                       <div className="col s12">
                         <Select 
                           ref={ref => { this.selectRef = ref; }}
                           blurInputOnSelect
                           defaultValue={this.state.topic}
-                          styles={customStyles} 
+                          styles={inlineStyle.customStyles} 
                           value={{label : this.state.topic}}
                           options={options} 
                           onChange={this.handleChange}
@@ -287,19 +238,19 @@ class App extends Component {
                   }
                   <div className="row">
                     <div className="col s12">
-                      <div className="row" style={rowStyle}>
+                      <div className="row" style={inlineStyle.rowStyle}>
                         <div className="col s6">
                           <button id="yesBtn"
-                            style={btnStyle} 
+                            style={inlineStyle.btnStyle} 
                             className={this.state.correct ? 'waves-effect waves-light btn-large green': 'waves-effect waves-light btn-large grey'} 
-                            onClick={this.setCorrect}><i style={symbolStyle} className="material-icons left">check</i>yes
+                            onClick={this.setCorrect}><i style={inlineStyle.symbolStyle} className="material-icons left">check</i>yes
                           </button>
                         </div>
                         <div className="col s6">
                           <button id="noBtn"
-                            style={btnStyle} 
+                            style={inlineStyle.btnStyle} 
                             className={this.state.correct ? 'waves-effect waves-light btn-large grey': 'waves-effect waves-light btn-large red'} 
-                            onClick={this.setFalse}><i style={symbolStyle} className="material-icons left">clear</i>no
+                            onClick={this.setFalse}><i style={inlineStyle.symbolStyle} className="material-icons left">clear</i>no
                           </button>
                         </div>
                       </div>
@@ -307,12 +258,12 @@ class App extends Component {
                   </div>
                 </div>
               </div>
-              <div className="col m2 hide-on-small-only" style={arrowStyle}>
+              <div className="col m2 hide-on-small-only" style={inlineStyle.arrowStyle}>
                 <button id="arrowUp" 
                   onClick={this.indexUp} 
                   disabled={this.state.index===this.state.data.length-1?'disabled' : null} 
                   className="btn-floating btn-large waves-effect waves-light blue">
-                    <i style={arrowTxtStyle} className=" material-icons">keyboard_arrow_right</i>
+                    <i style={inlineStyle.arrowTxtStyle} className=" material-icons">keyboard_arrow_right</i>
                 </button>
               </div>
             </div>
@@ -320,8 +271,8 @@ class App extends Component {
         </div>
       );
     } else {
-      return(
-        <div style={loadingStyle}><img src={load} alt="Loading..." style={loadImg} height="100" width="100"/></div>
+      return (
+        <div style={inlineStyle.loadingStyle}><img src={load} alt="Loading..." style={inlineStyle.loadImg} height="100" width="100"/></div>
       )
     }
   }
